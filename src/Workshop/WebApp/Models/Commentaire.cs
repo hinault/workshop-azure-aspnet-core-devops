127c2c2035fda6a8bbf451b60b2291608cfefcc1
hinault <ngdonfack@gmail.com>
2020-01-23 14:54:56 -0500

    Ajout classe commentaire

---
+++ src/Workshop/WebApp/Models/Commentaire.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Commentaire
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DateCommentaire { get; set; }
+    }
+}
