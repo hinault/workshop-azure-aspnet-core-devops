aeab5c495af4cb2b1424364e66d7c06a335f8fd4
hinault <ngdonfack@gmail.com>
2020-01-24 12:21:14 -0500

    Solution étape 2

---
+++ Etape 2 - Ajouter Entity Framewor Core/Workshop/WebApp/Models/Commentaire.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace WebApp.Models
+{
+    public class Commentaire
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Email { get; set; }
+
+        public string Texte { get; set; }
+
+        public DateTime DateCommentaire { get; set; }
+    }
+}
