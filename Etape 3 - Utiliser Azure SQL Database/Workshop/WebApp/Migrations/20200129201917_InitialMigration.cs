70db21b86b431495994f0c2e457abf9f79d01ebf
hinault <ngdonfack@gmail.com>
2020-01-29 16:11:59 -0500

    Ajout projet étape 3

---
+++ Etape 3 - Utiliser Azure SQL Database/Workshop/WebApp/Migrations/20200129201917_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commentaires",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    Texte = table.Column<string>(nullable: false),
+                    DateCommentaire = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commentaires", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commentaires");
+        }
+    }
+}
---
+++ Etape 3 - Utiliser Azure SQL Database/Workshop/WebApp/Models/Commentaire.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Commentaire
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        [Required]
+        [Display(Name = "Adresse Mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Commemtaire")]
+        public string Texte { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateCommentaire { get; set; }
+    }
+}
